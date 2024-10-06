2fbd85d985d0b80f4e325dd0a4928f91046605fe
Forrest Miller <forrestmillerj@gmail.com>
2024-10-06 08:05:55 -0500

    outputs

--- infra/modules/aws/lambda/_outputs.tf
+++ infra/modules/aws/lambda/_outputs.tf
@@ -5,3 +5,8 @@ output "lambda_name_hello" {
 output "lambda_arn_hello" {
   value = aws_lambda_function.hello.arn
 }
+
+output "lambda_invoke_arn_hello" {
+  value = aws_lambda_function.hello.invoke_arn
+}
+
---
+++ infra/modules/aws/s3/_outputs.tf
@@ -0,0 +1,3 @@
+output "s3_bucket_id_example" {
+  value = aws_s3_bucket.example.id
+}
